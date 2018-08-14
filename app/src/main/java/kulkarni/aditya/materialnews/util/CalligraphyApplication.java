79d8b27f830fb10c842282d1b08cc4ac2802f104
adikul30 <adi.kul30@gmail.com>
2018-08-14 11:39:31 +0530

    rebase with master | dependency updates

--- app/src/main/java/kulkarni/aditya/materialnews/util/CalligraphyApplication.java
+++ app/src/main/java/kulkarni/aditya/materialnews/util/CalligraphyApplication.java
@@ -3,8 +3,10 @@ package kulkarni.aditya.materialnews.util;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 
 import io.fabric.sdk.android.Fabric;
+import kulkarni.aditya.materialnews.BuildConfig;
 import kulkarni.aditya.materialnews.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
@@ -24,7 +26,12 @@ public class CalligraphyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        // Initialize Fabric with the debug-disabled crashlytics.
+        Fabric.with(this, crashlyticsKit);
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/ProductSans-Regular.ttf")
                 .setFontAttrId(R.attr.fontPath)
